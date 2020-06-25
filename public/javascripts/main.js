d822e3e537028b8feac6435a96e63d3c7df3e1f0
SDCard <sam.drage@yahoo.com>
2020-06-25 08:36:36 +0100

    basic synth manipulation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
-const synth = new Tone.Synth().toMaster();
+let synth = new Tone.Synth().toMaster();
+
+const typeSelect = document.querySelector("#type-select");
+
+const attackCtl = document.querySelector("#attackctrl");
+const decayCtl = document.querySelector("#decayctrl");
+const sustainCtl = document.querySelector("#sustainctrl");
+const releaseCtl = document.querySelector("#releasectrl");
 
 function loadpage() {
     Tone.start();
@@ -8,10 +15,17 @@ function loadpage() {
 }
 
 async function playTone() {
+
+    synth.oscillator.type = typeSelect.value;
+    synth.envelope.attack = attackCtl.value;
+    synth.envelope.decay = decayCtl.value;
+    synth.envelope.sustain = sustainCtl.value;
+    synth.envelope.release = releaseCtl.value;
+
     synth.triggerAttackRelease("C4", "8n");
 }
 
-document.querySelector("#playBttn").addEventListener('click', playTone);
+document.querySelector("#testBttn").addEventListener('click', playTone);
 
 window.addEventListener('load', loadpage);
 
