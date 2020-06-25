f8a769ffdc270aa97ef3b1289d974156dfd7159f
SDCard <sam.drage@yahoo.com>
2020-06-25 14:50:58 +0100

    Fixed pitching issues

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -3,7 +3,7 @@
 const canvas = document.querySelector("#sequencer");
 const ctx = canvas.getContext('2d');
 
-const keys = ['C4', 'D4', 'E4', 'F4', 'G4', 'A5', 'B5', 'C5'];
+const keys = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 
 ctx.strokeRect(0, 0, 800, 200);
 ctx.font = '16px serif';
@@ -53,26 +53,7 @@ class NoteLine  {
     getKey()    {
         let numKey = parseInt(this.startY/25);
         console.log(numKey);
-        switch (numKey) {
-            case 0:
-                return "C4";
-            case 1:
-                return "D4";
-            case 3:
-                return "E4";
-            case 4:
-                return "F4";
-            case 5:
-                return "G4";
-            case 6:
-                return "A5";
-            case 7:
-                return "B5";
-            case 8:
-                return "C5";
-            default:
-                return "C4";
-        }
+        return keys[numKey];
     }
 
     getStartBeat()  {
